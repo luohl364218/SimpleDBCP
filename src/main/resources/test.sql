76f76023e24dc7795dc2b4835c99f8c208b5a248
luohl364218 <luohl364218@gmail.com>
2018-04-27 21:34:17 +0800

    【9】添加测试代码以及所用的数据库生成语句

---
+++ src/main/resources/test.sql
@@ -0,0 +1,30 @@
+-- MySQL dump 10.13  Distrib 5.7.12, for Win64 (x86_64)
+--
+-- Host: localhost    Database: test
+-- ------------------------------------------------------
+-- Server version	5.6.34-log
+
+--
+-- Table structure for table `items`
+--
+
+DROP TABLE IF EXISTS `items`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `items` (
+  `ID` int(11) NOT NULL,
+  `NAME` varchar(45) DEFAULT NULL,
+  `PRICE` int(11) DEFAULT NULL,
+  PRIMARY KEY (`ID`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `items`
+--
+
+LOCK TABLES `items` WRITE;
+/*!40000 ALTER TABLE `items` DISABLE KEYS */;
+INSERT INTO `items` VALUES (1,'heylink',100),(2,'chicken',200),(3,'dog',234);
+/*!40000 ALTER TABLE `items` ENABLE KEYS */;
+UNLOCK TABLES;
